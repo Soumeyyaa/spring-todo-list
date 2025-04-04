cdc3af7654c2be83bf71872dc687b3d2799d1730
Soumeyyaa <saidi.soumeyaa@gmail.com>
2025-04-04 17:50:56 +0200

    Initialized services

--- src/main/java/fr/api/springtodolist/services/TaskService.java
+++ src/main/java/fr/api/springtodolist/services/TaskService.java
@@ -1,2 +1,16 @@
-package fr.api.springtodolist.services;public class TaskService {
+package fr.api.springtodolist.services;
+
+import fr.api.springtodolist.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
 }
