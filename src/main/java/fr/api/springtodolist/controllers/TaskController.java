ce26266c0b354495036aacc8f8748169be502405
Soumeyyaa <saidi.soumeyaa@gmail.com>
2025-04-04 17:52:46 +0200

    Initialized controllers

--- src/main/java/fr/api/springtodolist/controllers/TaskController.java
+++ src/main/java/fr/api/springtodolist/controllers/TaskController.java
@@ -1,2 +1,16 @@
-package fr.api.springtodolist.controllers;public class TaskController {
+package fr.api.springtodolist.controllers;
+
+import fr.api.springtodolist.services.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskController {
+
+    private final TaskService taskService;
+
+    @Autowired
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
 }
--- src/main/java/fr/api/springtodolist/controllers/UserController.java
+++ src/main/java/fr/api/springtodolist/controllers/UserController.java
@@ -1,2 +1,16 @@
-package fr.api.springtodolist.controllers;public class UserController {
+package fr.api.springtodolist.controllers;
+
+import fr.api.springtodolist.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 }
